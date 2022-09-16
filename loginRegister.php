4962a735bac6c2643671fa2751663af0320ca146
Maria3k <maria3kmorales05@gmail.com>
2022-09-16 08:52:17 -0300

    cambio de rama
    cambio de rama

--- loginRegister.php
+++ loginRegister.php
@@ -5,9 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Museo</title>
-    <link rel="icon" type="image/png" href="assets/imagenes/Logotipo.png">
+    <link rel="icon" type="image/png" href="assets/imagenes/favicon.png">
 
-    <link rel="stylesheet" href="assets\estilos\estilos.css">
+    <link rel="stylesheet" href="assets/estilos/estilos.css">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -45,7 +45,7 @@
                 <form action="php/login_usuario_be.php" method="POST" class="formulario_login">
 
                     <h2>Iniciar Sesión</h2>
-                    <img class = "logo" src="assets\imagenes\Logotipo.png" height="25%" width="25%">
+                    <img class = "logo" src="assets/imagenes/favicona.png" height="30%" width="30%">
                     <input type="text" placeholder="Nombre de Usuario" name="nombre_usuario" required>
                     <input type="password" placeholder="Contraseña" name="password" required>
                     <button>Iniciar Sesión</button>
@@ -55,7 +55,7 @@
                 <form action="php/registro_usuario_be.php" method="POST" class="formulario_register">
 
                     <h2>Registrarse</h2>
-                    <img class = "logo" src="assets\imagenes\Logotipo.png" height="25%" width="25%">
+                    <img class = "logo" src="assets/imagenes/favicona.png" height="25%" width="25%">
                     <input type="text" placeholder="Nombre Completo" name="nombre_completo" required>
                     <input type="text" placeholder="Dirección de Email" name="correo_electronico" required>
                     <input type="text" placeholder="Nombre de Usuario" name="nombre_usuario" required>
