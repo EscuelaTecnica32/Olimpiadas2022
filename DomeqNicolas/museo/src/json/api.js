5edcf518c72aaa89fb86d810b9469fa0509996a9
Gonzalo Consorti <consor92@gmail.com>
2022-09-19 16:40:51 -0300

    Diseño mobile

--- DomeqNicolas/museo/src/json/api.js
+++
@@ -1,9 +0,0 @@
-const lista = async () =>{
-    const options = { method: 'GET' };
-
-    const response = await fetch('http://olimpiadaset32.000webhostapp.com/index.php?consulta=obtenerRecorridos',options)
-
-    return await response.json()
-}
-
-export default lista
